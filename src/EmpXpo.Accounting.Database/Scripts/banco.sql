70f07db4ac89b39c28809a0abb494469cc696e04
XavierHub <bestxavier16@yahoo.com.br>
2024-07-16 12:07:49 -0300

    Update banco.sql

--- src/EmpXpo.Accounting.Database/Scripts/banco.sql
+++ src/EmpXpo.Accounting.Database/Scripts/banco.sql
@@ -1,26 +1,3 @@
-﻿USE [master]
-GO
-
-CREATE DATABASE [DbCashFlow]
-GO
-
-USE [DbCashFlow]
-GO
-
-CREATE TABLE [dbo].[CashFlow]
-(
-   [Id]                    INT           PRIMARY KEY  IDENTITY   NOT NULL,	
-   [Type]                  INT                                   NOT NULL,
-   [Amount]                DECIMAL(18, 2)                        NOT NULL,
-   [Description]           VARCHAR(100)                          NOT NULL,
-   [CreatedOn]             SMALLDATETIME                         NOT NULL,    
-   CHECK ([Type]=(0) OR [Type]=(1)), -- 0:'Debit', 1:'Credit'
-)
-GO
-CREATE INDEX [Ind_CashFlow_1] ON [dbo].[CashFlow] ([CreatedOn])
-GO
-
-
 CREATE PROCEDURE [dbo].[CashFlowReport](   
    @StartDate     SMALLDATETIME,
    @EndDate       SMALLDATETIME
@@ -32,7 +9,7 @@ BEGIN
       /* Ex.:
             Procedure que retorna o valor de debito e credito de acordo com ranger das datas especificadas
 
-            EXEC [dbo].[CashFlowReport] @StartDate='20240713', @EndDate = '20240715';
+            EXEC [dbo].[CashFlowReport] @StartDate='20240716', @EndDate = '20240717';
             EXEC [dbo].[CashFlowReport] @StartDate='20240701', @EndDate = '20240801';
       */      
       
@@ -42,40 +19,19 @@ BEGIN
          SELECT 
                CASE WHEN cf.[Type] = 0 THEN 'Debit' ELSE 'Credit' END 'Type',
                cf.[Amount]
-         FROM [dbo].[CashFlow] cf
+         FROM [dbo].[CashFlow]  cf
          WITH (NOLOCK)
         WHERE cf.[CreatedOn] BETWEEN @StartDate AND @EndDate
       )
       SELECT 
-              [Debit], 
-              [Credit],
-              [Debit] + [Credit] AS Total
+              ISNULL([Debit], 0)                       AS [Debit], 
+              ISNULL([Credit], 0)                      AS [Credit] ,
+              ISNULL([Debit], 0) + ISNULL([Credit],0)  AS [Total]
         FROM CashFlow_cte
        PIVOT (
                SUM([Amount])
                FOR [Type] IN([Debit], [Credit])
              ) AS pvt
 
-    SET NOCOUNT OFF
-END
-
-GO
-
-CREATE PROCEDURE [dbo].[CashFlowReportDate]
-AS
-BEGIN
-      SET NOCOUNT ON;
-      /* Ex.:
-            Procedure que retorna as datas disponíveis para geração do relatório
-
-            EXEC [dbo].[CashFlowReportDate]            
-      */      
-     
-       SELECT DISTINCT
-              FORMAT(cf.CreatedOn, 'yyyy-MM-dd 00:00:00') CreatedOn
-         FROM [dbo].[CashFlow] cf
-         WITH (NOLOCK)
-     GROUP BY CreatedOn
-
-     SET NOCOUNT OFF
+    SET NOCOUNT OFF;
 END
